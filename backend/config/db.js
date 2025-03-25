d89ded0fc7f5ff2aa8e9c93bb0dcf55c12cc7e8e
Edwin Molina Lap <mol13026@gmail.com>
2025-03-25 00:05:33 -0500

    Update enviroment

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,11 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = mysql.createPool({
-	host: process.env.MYSQLHOST,
-	user: process.env.MYSQLUSER,
-	password: process.env.MYSQLPASSWORD,
-	database: process.env.MYSQLDATABASE,
-	port: process.env.MYSQLPORT
+	host: process.env.MYSQLHOST || "localhost",
+	user: process.env.MYSQLUSER || 'root',
+	password: process.env.MYSQLPASSWORD || "",
+	database: process.env.MYSQLDATABASE || "DBMeta",
+	port: process.env.MYSQLPORT || 3306,
 });
 
 
